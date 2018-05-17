ded8f2d19b33c4a3d97564a405fb2d3294dc729a
Viraj Bhosale <viraj.bhosale@macys.com>
2018-05-17 21:07:55 +0530

    Bounce ball off the wall

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
@@ -8,7 +9,7 @@ var dy = -2;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
@@ -19,6 +20,13 @@ function draw() {
     drawBall();
     x += dx;
     y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
 
 setInterval(draw, 10);
